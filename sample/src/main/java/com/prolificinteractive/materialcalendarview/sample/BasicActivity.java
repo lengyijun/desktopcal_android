0e086e4639158bc27e0f822dc2a7b345ecfe0f1c
sjtu-lyj <sjtu5140809011@gmail.com>
2016-01-19 21:13:15 +0800

    dynamid search db

--- sample/src/main/java/com/prolificinteractive/materialcalendarview/sample/BasicActivity.java
+++ sample/src/main/java/com/prolificinteractive/materialcalendarview/sample/BasicActivity.java
@@ -1,8 +1,10 @@
 package com.prolificinteractive.materialcalendarview.sample;
 
+import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -13,9 +15,15 @@ import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
 import com.prolificinteractive.materialcalendarview.OnMonthChangedListener;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.filefilter.RegexFileFilter;
+import org.apache.commons.io.filefilter.TrueFileFilter;
+
+import java.io.File;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Collection;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -26,6 +34,7 @@ import butterknife.ButterKnife;
 public class BasicActivity extends AppCompatActivity implements OnDateSelectedListener, OnMonthChangedListener {
 
     private static final DateFormat FORMATTER = SimpleDateFormat.getDateInstance();
+    DBopenhelper dBopenhelper;
 
     @Bind(R.id.calendarView)
     MaterialCalendarView widget;
@@ -47,6 +56,42 @@ public class BasicActivity extends AppCompatActivity implements OnDateSelectedLi
 
 //        初始化设置
         onDateSelected(widget, widget.getSelectedDate(), true);
+    }
+
+    public String find_path_from_db() {
+         dBopenhelper=new DBopenhelper(this,"main_tb",null,1);
+         SQLiteDatabase path_db=dBopenhelper.getReadableDatabase();
+         Cursor path_cursor=path_db.query("main_tb",null,"st_name like ?",new String[]{"db_path"},null,null,null);
+         path_cursor.moveToFirst();
+         String aboslute_path=path_cursor.getString(path_cursor.getColumnIndex("st_val"));
+         try {
+             db = SQLiteDatabase.openDatabase(aboslute_path,null, 0);
+             db.close();
+         }catch (Exception e){
+              aboslute_path=find_path_from_whole_phone();
+              ContentValues values=new ContentValues();
+              values.put("st_val",aboslute_path);
+              path_db.update("main_tb",values,"st_name like ?",new String[]{"db_path"});
+         }
+         path_cursor.close();
+         path_db.close();
+         return aboslute_path;
+    }
+
+    public String find_path_from_whole_phone(){
+        String ss="";
+        Collection<File> files= FileUtils.listFiles(Environment.getExternalStorageDirectory(),
+                new RegexFileFilter("calendar.db"),
+                TrueFileFilter.TRUE
+        );
+
+        for(File f:files){
+            if(f.getName().equals("calendar.db")){
+                ss=f.getAbsolutePath();
+                break;
+            }
+        }
+        return ss;
     }
 
     public String [] process_date(CalendarDay date){
@@ -78,7 +123,8 @@ public class BasicActivity extends AppCompatActivity implements OnDateSelectedLi
 
         String [] whereArgs=process_date(date);
         try{
-        db = SQLiteDatabase.openDatabase("/storage/emulated/0/Android/data/nutstore.android/cache/objectcache/1/calendar.db", null, 0);
+        db = SQLiteDatabase.openDatabase(find_path_from_db(), null, 0);
+//            db = SQLiteDatabase.openDatabase("/storage/emulated/0/Android/data/nutstore.android/cache/objectcache/1/calendar.db", null, 0);
         cursor = db.query("item_table", new String[]{"it_content", "it_unique_id"}, "it_unique_id like ?", whereArgs, null, null, null);
 
         cursor.moveToFirst();
@@ -89,7 +135,7 @@ public class BasicActivity extends AppCompatActivity implements OnDateSelectedLi
             textView.setText(ss);
         }
         }catch (Exception e){
-            textView.setText("查询不到这条记录");
+            textView.setText("(查询不到这条记录)");
         }
     }
 
